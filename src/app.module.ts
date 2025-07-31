dfcf65f5ed3b94e3edf87e57cdc3689bdb4e6eb3
Camila Maron <camila.maron93@gmail.com>
2025-07-31 18:23:04 -0300

    FOC-190: Backend > Configurar entorno (.env, CORS, conexión DB)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const AppDataSource = require('./data-source');
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRoot(AppDataSource.options),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const frontendOrigin = configService.get<string>('FRONTEND_ORIGIN');
+
+  app.enableCors({
+    origin: frontendOrigin,
+    credentials: true, // Si usás cookies o headers con auth
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
